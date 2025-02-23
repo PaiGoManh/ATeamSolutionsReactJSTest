cb1fdaa4002b96aaa50aec3beba20ef9287a170f
Rahul Sajeevan <144695000+PaiGoManh@users.noreply.github.com>
2025-02-24 03:13:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div className='flex justify-center gap-[5%] mt-[5%] '>
-      <button className='border border-2 p-2 '>Duplicate</button>
-      <button className='border border-2 p-2'>Sort</button>
-      <button className='border border-2 p-2'>Table</button>
-      <button className='border border-2 p-2'>Map</button>
+      <button className='border border-2 p-2 '> Duplicate</button>
+      <button className='border border-2 p-2'> Sort</button>
+      <button className='border border-2 p-2'> Table</button>
+      <button className='border border-2 p-2'> Map</button>
     </div>
   )
 }
